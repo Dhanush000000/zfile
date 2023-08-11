01cf3452bb4dbdf5d79c3318bcb3b54eae9ee47d
Dhanush000000 <100513826+Dhanush000000@users.noreply.github.com>
2023-08-11 09:21:47 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -28,7 +28,7 @@ class _BotCommands:
         self.StatsCommand = [f'stats{CMD_SUFFIX}', 's']
         self.HelpCommand = f'help{CMD_SUFFIX}'
         self.LogCommand = f'log{CMD_SUFFIX}'
-        self.ShellCommand = f'shell{CMD_SUFFIX}'
+        self.ShellCommand = [f'shell{CMD_SUFFIX}', f'sh{CMD_SUFFIX}']
         self.EvalCommand = f'eval{CMD_SUFFIX}'
         self.ExecCommand = f'exec{CMD_SUFFIX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_SUFFIX}'
@@ -38,7 +38,7 @@ class _BotCommands:
         self.RssCommand = f'rss{CMD_SUFFIX}'
         self.CategorySelect = f'catsel{CMD_SUFFIX}'
         self.RmdbCommand = f'rmdb{CMD_SUFFIX}'
-        self.MLocalUploadCommand = f'mupload{CMD_SUFFIX}'
-        self.LLocalUploadCommand = f'lupload{CMD_SUFFIX}'
+        self.MLocalUploadCommand = [f'mupload{CMD_SUFFIX}', f'up{CMD_SUFFIX}']
+        self.LLocalUploadCommand = [f'lupload{CMD_SUFFIX}', f'lup{CMD_SUFFIX}']
 
 BotCommands = _BotCommands()
